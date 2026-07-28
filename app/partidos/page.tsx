6de2aa50fd9c7ea2fc6a831d36a03e5252f4afcb
ph-ayep <ayepanella2@gmail.com>
2026-07-28 17:06:40 -0300

    Update page.tsx

--- app/partidos/page.tsx
+++ app/partidos/page.tsx
@@ -1,1 +1,24 @@
+import Link from "next/link";
 
+export default function PartidosPage() {
+  const partidos = [
+    { id: "1", titulo: "River vs Boca" },
+    { id: "2", titulo: "Argentina vs Brasil" },
+  ];
+
+  return (
+    <main>
+      <h1>Partidos</h1>
+
+      <ul>
+        {partidos.map((partido) => (
+          <li key={partido.id}>
+            <Link href={`/partidos/${partido.id}`}>
+              {partido.titulo}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
